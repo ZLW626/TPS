5352cc147b48d748f17e5ff892921188b0428047
liwei <2392867657@qq.com>
2019-07-25 21:17:29 +0800

    first commit

---
+++ Common/Conf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Script.Common
+{
+    public class Conf
+    {
+        public static short MSG_CS_LOGIN = 0x1001;
+
+        public static int NET_HEAD_LENGTH_SIZE = 4;
+        public static int NET_SID_CID_LENGTH_SIZE = 2;
+    }
+}
---
+++ Common/message.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using UnityEngine;
+namespace Assets.Script.Common
+{
+    public class MsgCSLogin
+    {
+        private int name;
+        private int password;
+
+        public MsgCSLogin(int name, int password)
+        {
+            this.name = name;
+            this.password = password;
+        }
+
+        public byte[] Marshal()
+        {
+            //byte[] bytes;
+            //using (MemoryStream memoryStream = new MemoryStream())
+            MemoryStream memoryStream = new MemoryStream();
+            //{
+                //using (BinaryWriter binaryWriter = new BinaryWriter(memoryStream))
+                BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+                //{
+                    //int datalen = Conf.NET_HEAD_LENGTH_SIZE + Conf.NET_SID_CID_LENGTH_SIZE + name.Length + password.Length;
+                    int datalen = Conf.NET_HEAD_LENGTH_SIZE + Conf.NET_SID_CID_LENGTH_SIZE + 8;
+                    binaryWriter.Write(datalen);
+                    binaryWriter.Write(Conf.MSG_CS_LOGIN);
+                    binaryWriter.Write(name);
+                    binaryWriter.Write(password);
+                    binaryWriter.Close();
+                    //bytes = (byte[])memoryStream.ToArray().Clone();
+                    return memoryStream.ToArray();
+                //}
+            //}
+            
+            //return bytes;
+        }
+    }
+}
